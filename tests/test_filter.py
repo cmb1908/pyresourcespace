6af2323fffe53da8ad4b0d7b980bdc0ada9e2031
Craig Brown <cmb1908@gmail.com>
2025-02-25 20:48:34 +1100

    Filter adjust

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -19,16 +19,20 @@ def test_filter_label(server_connect):
 def test_filter_toi_irn(server_connect):
     results = orm.Asset.query("filter 'powerhouse-toi:irn(value=\\'234\\')'")
 
-    check.greater_equal(len(results), 5, f"Expecting at least 5 results, got {len(results)}")
+    check.greater_equal(
+        len(results), 5, f"Expecting at least 5 results, got {len(results)}"
+    )
 
 
 def test_filter_toi_irn_in_toi(server_connect):
-    results = orm.Asset.query("filter 'powerhouse-toi:irn(value=\\'234\\')' and asset has parent collection 5642820")
+    results = orm.Asset.query(
+        "filter 'powerhouse-toi:irn(value=\\'234\\')' and asset has parent collection 5642820"
+    )
 
     check.equal(len(results), 3, f"Expecting 3 results, got {len(results)}")
 
 
-@pytest.mark.timeout(2)
+@pytest.mark.timeout(5)
 def test_filter_query(query_str):
 
     orm.Asset.query(query_str)
